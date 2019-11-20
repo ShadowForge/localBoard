1819633a5f1b62d6b2d38742f559c71ce7ea610c
cerezora <cerezora@sonoma.edu>
2019-11-19 21:52:37 -0800

    "+" Button to new event- Intent

--- app/src/main/java/com/example/localboard/MainActivity.java
+++ app/src/main/java/com/example/localboard/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.localboard;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button addButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addButton = findViewById(R.id.AddEventButton);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,EventView.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
