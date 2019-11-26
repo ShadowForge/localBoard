d20b3588576812391eb9bf10c1b03cca184a53e8
Nicholas Tatman <nick.tatman@gmail.com>
2019-11-25 18:38:21 -0800

    Worthless Spinner Added for Sort

--- app/src/main/java/com/example/localboard/MainActivity.java
+++ app/src/main/java/com/example/localboard/MainActivity.java
@@ -7,7 +7,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -36,6 +38,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = findViewById(R.id.spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.sortChoices, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+
+
+
 
         recyclerView = findViewById(R.id.recyclerView);
 
---
+++ app/src/main/java/com/example/localboard/SpinnerActivity.java
@@ -0,0 +1,17 @@
+package com.example.localboard;
+
+import android.view.View;
+import android.widget.AdapterView;
+
+public class SpinnerActivity extends MainActivity implements AdapterView.OnItemSelectedListener {
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+}
