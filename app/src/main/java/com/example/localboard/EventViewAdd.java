547381cda4a76e4985d67dafb5f564f08c6c0fe5
cerezora <cerezora@sonoma.edu>
2019-12-02 17:22:57 -0800

    Added Saved Button and worked on Add Event

--- app/src/main/java/com/example/localboard/EventViewAdd.java
+++ app/src/main/java/com/example/localboard/EventViewAdd.java
@@ -1,6 +1,9 @@
 package com.example.localboard;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +16,15 @@ public class EventViewAdd extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_view_add);
+
+        Button addbutton = findViewById(R.id.AddYourEventButton);
+
+        addbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EventViewAdd.this, MainActivity.class));
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/localboard/MainActivity.java
+++ app/src/main/java/com/example/localboard/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -38,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         Spinner spinner = findViewById(R.id.spinner);
 // Create an ArrayAdapter using the string array and a default spinner layout
@@ -127,6 +129,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button savedbutton = findViewById(R.id.SavedEventsButton);
+
+        savedbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SavedEvents.class));
+            }
+        });
 
 
     }
