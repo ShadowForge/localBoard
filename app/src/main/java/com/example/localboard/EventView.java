e4d76bd9a6d7c8bb601ad0c0d2a21a6bc8eb1748
Nicholas Tatman <nick.tatman@gmail.com>
2019-12-03 22:03:51 -0800

    Saved is functional

--- app/src/main/java/com/example/localboard/EventView.java
+++ app/src/main/java/com/example/localboard/EventView.java
@@ -2,26 +2,36 @@ package com.example.localboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 
 public class EventView extends AppCompatActivity {
 
     private TextView description;
     private ImageView image;
-    private TextView cost, time, title, link;
+    private TextView cost, time, title, link, address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_view);
 
-        EventModel receivedModel = (EventModel) getIntent().getSerializableExtra("serializable_data");
+        final EventModel receivedModel = (EventModel) getIntent().getSerializableExtra("serializable_data");
 
         description = findViewById(R.id.descriptionTextView);
         cost = findViewById(R.id.costBox);
@@ -29,12 +39,57 @@ public class EventView extends AppCompatActivity {
         title = findViewById(R.id.titleBox);
         link = findViewById(R.id.linkTextView);
         image = findViewById(R.id.eventImageView);
+        address = findViewById(R.id.addressBox);
+        Button saveButton = findViewById(R.id.saveButton);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String fileName = "myFile.txt";
+                String whichIds = "";
+
+                try {
+                    FileInputStream fileIn= openFileInput(fileName);
+                    InputStreamReader InputRead= new InputStreamReader(fileIn);
+                    char[] inputBuffer= new char[100];
+                    String s="";
+                    int charRead;
+                    while ((charRead=InputRead.read(inputBuffer))>0) {
+                        // char to string conversion
+                        String readstring = String.copyValueOf(inputBuffer,0,charRead);
+                        Log.d("Watchamacallit", readstring);
+                            whichIds += readstring;
+                    }
+                    InputRead.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+
+                String data= String.valueOf(receivedModel.getId());
+                try {
+                    FileOutputStream fOut = openFileOutput(fileName, Context.MODE_PRIVATE);
+                    OutputStreamWriter outputWriter=new OutputStreamWriter(fOut);
+                    //outputWriter.write(data);
+                    outputWriter.write(whichIds + " " + data);
+                    outputWriter.close();
+                    Toast.makeText(getBaseContext(), "File saved successfully!",
+                            Toast.LENGTH_SHORT).show();
+                }
+                catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        });
 
         title.setText(receivedModel.getTitle());
         cost.setText(String.valueOf(receivedModel.getCost()));
         time.setText(receivedModel.getTime());
         link.setText(receivedModel.getLink());
         description.setText(receivedModel.getDescription());
+        address.setText(receivedModel.getAddress());
         Glide.with(this)
                 .load(receivedModel.getImage())
                 .into(image);
--- app/src/main/java/com/example/localboard/SavedEvents.java
+++ app/src/main/java/com/example/localboard/SavedEvents.java
@@ -1,14 +1,149 @@
 package com.example.localboard;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+import static java.lang.System.in;
 
 public class SavedEvents extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private String url;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved_events);
+
+        url = getString(R.string.baseUrl);
+        final RequestQueue queue = Volley.newRequestQueue(SavedEvents.this);
+        recyclerView = findViewById(R.id.recyclerViewSaved);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        final OnItemClickRecyclerListener listener = new OnItemClickRecyclerListener() {
+            @Override
+            public void onRecyclerItemClick(EventModel model) {
+                Log.d("word", url+"?id=eq."+ model.getId());
+                volleyRequestData(url+"?id=eq."+ model.getId(), queue);
+
+                //do some stuff after item click
+                return;
+            }
+        };
+
+        String fileName = "myFile.txt";
+        String whichIds = "";
+
+        try {
+            FileInputStream fileIn=openFileInput(fileName);
+            InputStreamReader InputRead= new InputStreamReader(fileIn);
+            char[] inputBuffer= new char[100];
+            String s="";
+            int charRead;
+            Boolean first = true;
+            while ((charRead=InputRead.read(inputBuffer))>0) {
+                // char to string conversion
+                String readstring = String.copyValueOf(inputBuffer,0,charRead);
+                Log.d("Watchamacallit", readstring);
+                Scanner scanner = new Scanner(readstring);
+                while(scanner.hasNext()) {
+                    if (first == false) {
+                        whichIds += ",id.eq." + scanner.nextInt();
+                    } else {
+                        whichIds += "id.eq." + scanner.nextInt();
+                        first = false;
+                    }
+                }
+            }
+            InputRead.close();
+            volleyRequest(url+"?select=id,image,title" +"&or=(" + whichIds+")", queue, listener);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+        volleyRequest(url+"?select=id,image,title" +"&or=(" + whichIds+")", queue, listener);
+
     }
+
+    void volleyRequest(final String url, RequestQueue queue, final OnItemClickRecyclerListener listener) {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        //Log.d("word", response);
+                        Log.d("word", url);
+                        Gson g = new Gson();
+                        JsonArray jsonArray = g.fromJson(response, JsonArray.class);
+                        EventModelList myList = new EventModelList(jsonArray);
+                        myAdapter newAdapter = new myAdapter(myList.getEvents(), listener);
+                        recyclerView.setAdapter(newAdapter);
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Oh no", "Rip");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+    void volleyRequestData(String url, RequestQueue queue) {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Log.d("word", "It is happening");
+                        //Log.d("word", response);
+                        Gson g = new Gson();
+                        JsonArray jsonArray = g.fromJson(response, JsonArray.class);
+                        EventModelList myList = new EventModelList(jsonArray);
+                        Intent intent = new Intent(SavedEvents.this, EventView.class);
+                        intent.putExtra("serializable_data", myList.getEvents().get(0));
+                        startActivity(intent);
+
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Oh no", "Rip");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
 }
+
